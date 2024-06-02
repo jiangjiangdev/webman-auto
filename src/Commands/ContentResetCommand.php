380676efbf23d51b3fcc9eecaa978c5be5ad5242
JiangJiang <jiang@jiang.jiang>
2024-06-02 11:18:50 +0800

    First commit

---
+++ src/Commands/ContentResetCommand.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace jiangjiangdev\WebmanAuto\Commands;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ContentResetCommand extends Command
+{
+    protected static $defaultName = 'auto:reset';
+    protected static $defaultDescription = 'Auto reset all config';
+
+    private const STUB_PATH = __DIR__ . '/../stubs/config/';
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        // Reset config
+        // app.php
+        $stubContent = file_get_contents(self::STUB_PATH . 'app.stub');
+        $targetFolderPath = base_path() . "/config/app.php";
+        $this->checkAndCreateFile($targetFolderPath, $stubContent, true);
+
+        // container.php
+        $stubContent = file_get_contents(self::STUB_PATH . 'container.stub');
+        $targetFolderPath = base_path() . "/config/container.php";
+        $this->checkAndCreateFile($targetFolderPath, $stubContent, true);
+
+        // database.php
+        $stubContent = file_get_contents(self::STUB_PATH . 'database.stub');
+        $targetFolderPath = base_path() . "/config/database.php";
+        $this->checkAndCreateFile($targetFolderPath, $stubContent, true);
+
+        // process.php
+        $stubContent = file_get_contents(self::STUB_PATH . 'process.stub');
+        $targetFolderPath = base_path() . "/config/process.php";
+        $this->checkAndCreateFile($targetFolderPath, $stubContent, true);
+
+        // redis.php
+        $stubContent = file_get_contents(self::STUB_PATH . 'redis.stub');
+        $targetFolderPath = base_path() . "/config/redis.php";
+        $this->checkAndCreateFile($targetFolderPath, $stubContent, true);
+
+        // server.php
+        $stubContent = file_get_contents(self::STUB_PATH . 'server.stub');
+        $targetFolderPath = base_path() . "/config/server.php";
+        $this->checkAndCreateFile($targetFolderPath, $stubContent, true);
+
+        // session.php
+        $stubContent = file_get_contents(self::STUB_PATH . 'session.stub');
+        $targetFolderPath = base_path() . "/config/session.php";
+        $this->checkAndCreateFile($targetFolderPath, $stubContent, true);
+
+        // translation.php
+        $stubContent = file_get_contents(self::STUB_PATH . 'translation.stub');
+        $targetFolderPath = base_path() . "/config/translation.php";
+        $this->checkAndCreateFile($targetFolderPath, $stubContent, true);
+
+        // view.php
+        $stubContent = file_get_contents(self::STUB_PATH . 'view.stub');
+        $targetFolderPath = base_path() . "/config/view.php";
+        $this->checkAndCreateFile($targetFolderPath, $stubContent, true);
+
+        return self::SUCCESS;
+    }
+
+    // 檢查並創建檔案
+    private function checkAndCreateFile(string $fullPath, string $content, bool $force = false): bool
+    {
+        // 檢查檔案是否存在
+        if (file_exists($fullPath) && !$force) {
+            echo "File already exists: $fullPath\n";
+            return false;
+        }
+
+        // 取得資料夾路徑
+        $directoryPath = dirname($fullPath);
+
+        // 檢查資料夾是否存在，如果不存在則創建資料夾
+        if (!is_dir($directoryPath)) {
+            mkdir($directoryPath, 0777, true);
+            echo "資料夾已創建: $directoryPath\n";
+        }
+
+        // 創建空檔案
+        $fileHandle = fopen($fullPath, 'w');
+        if ($fileHandle) {
+            fwrite($fileHandle, $content);
+            fclose($fileHandle);
+            echo "檔案已創建: $fullPath\n";
+        } else {
+            echo "檔案創建失敗: $fullPath\n";
+        }
+
+        return true;
+    }
+}
