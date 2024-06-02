500c9b0ee49a697b449010a503326d4aa146eddc
JiangJiang <jiang@jiang.jiang>
2024-06-02 11:45:26 +0800

    feat: 增加name參數與env模板替換功能

--- src/Commands/ContentResetCommand.php
+++ src/Commands/ContentResetCommand.php
@@ -3,6 +3,7 @@
 namespace jiangjiangdev\WebmanAuto\Commands;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -11,54 +12,73 @@ class ContentResetCommand extends Command
     protected static $defaultName = 'auto:reset';
     protected static $defaultDescription = 'Auto reset all config';
 
-    private const STUB_PATH = __DIR__ . '/../stubs/config/';
+    private const STUB_PATH = __DIR__ . '/../stubs/';
+    private const CONFIG_STUB_PATH = __DIR__ . '/../stubs/config/';
+
+    protected function configure()
+    {
+        $this->addArgument('name', InputArgument::REQUIRED, 'App name');
+    }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $name = $input->getArgument('name');
         // Reset config
         // app.php
-        $stubContent = file_get_contents(self::STUB_PATH . 'app.stub');
+        $stubContent = file_get_contents(self::CONFIG_STUB_PATH . 'app.stub');
         $targetFolderPath = base_path() . "/config/app.php";
         $this->checkAndCreateFile($targetFolderPath, $stubContent, true);
 
         // container.php
-        $stubContent = file_get_contents(self::STUB_PATH . 'container.stub');
+        $stubContent = file_get_contents(self::CONFIG_STUB_PATH . 'container.stub');
         $targetFolderPath = base_path() . "/config/container.php";
         $this->checkAndCreateFile($targetFolderPath, $stubContent, true);
 
         // database.php
-        $stubContent = file_get_contents(self::STUB_PATH . 'database.stub');
+        $stubContent = file_get_contents(self::CONFIG_STUB_PATH . 'database.stub');
         $targetFolderPath = base_path() . "/config/database.php";
         $this->checkAndCreateFile($targetFolderPath, $stubContent, true);
 
         // process.php
-        $stubContent = file_get_contents(self::STUB_PATH . 'process.stub');
+        $stubContent = file_get_contents(self::CONFIG_STUB_PATH . 'process.stub');
         $targetFolderPath = base_path() . "/config/process.php";
         $this->checkAndCreateFile($targetFolderPath, $stubContent, true);
 
         // redis.php
-        $stubContent = file_get_contents(self::STUB_PATH . 'redis.stub');
+        $stubContent = file_get_contents(self::CONFIG_STUB_PATH . 'redis.stub');
         $targetFolderPath = base_path() . "/config/redis.php";
         $this->checkAndCreateFile($targetFolderPath, $stubContent, true);
 
         // server.php
-        $stubContent = file_get_contents(self::STUB_PATH . 'server.stub');
+        $stubContent = file_get_contents(self::CONFIG_STUB_PATH . 'server.stub');
         $targetFolderPath = base_path() . "/config/server.php";
         $this->checkAndCreateFile($targetFolderPath, $stubContent, true);
 
         // session.php
-        $stubContent = file_get_contents(self::STUB_PATH . 'session.stub');
+        $stubContent = file_get_contents(self::CONFIG_STUB_PATH . 'session.stub');
         $targetFolderPath = base_path() . "/config/session.php";
         $this->checkAndCreateFile($targetFolderPath, $stubContent, true);
 
         // translation.php
-        $stubContent = file_get_contents(self::STUB_PATH . 'translation.stub');
+        $stubContent = file_get_contents(self::CONFIG_STUB_PATH . 'translation.stub');
         $targetFolderPath = base_path() . "/config/translation.php";
         $this->checkAndCreateFile($targetFolderPath, $stubContent, true);
 
         // view.php
-        $stubContent = file_get_contents(self::STUB_PATH . 'view.stub');
+        $stubContent = file_get_contents(self::CONFIG_STUB_PATH . 'view.stub');
         $targetFolderPath = base_path() . "/config/view.php";
+        $this->checkAndCreateFile($targetFolderPath, $stubContent, true);
+
+        // .env
+        $stubContent = file_get_contents(self::STUB_PATH . 'env.stub');
+        $targetFolderPath = base_path() . "/.env";
+        // 替換名稱
+        $stubContent = str_replace('{{name}}', $name, $stubContent);
+        $this->checkAndCreateFile($targetFolderPath, $stubContent, true);
+        
+        // .gitignore
+        $stubContent = file_get_contents(self::STUB_PATH . 'gitignore.stub');
+        $targetFolderPath = base_path() . "/.gitignore";
         $this->checkAndCreateFile($targetFolderPath, $stubContent, true);
 
         return self::SUCCESS;
