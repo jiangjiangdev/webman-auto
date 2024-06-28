082e0e04ecf517805c2fe4e2e3d2c064b62eb8e5
JiangJiang <jiang@jiang.jiang>
2024-06-28 22:04:22 +0800

    reset

---
+++ src/config/plugin/jiangjiangdev/webman-auto/command.php
@@ -0,0 +1,9 @@
+<?php
+
+use Jiangjiangdev\WebmanAuto\Commands\ContentResetCommand;
+use Jiangjiangdev\WebmanAuto\Commands\ResourceCreateCommand;
+
+return [
+    ContentResetCommand::class,
+    ResourceCreateCommand::class,
+];
