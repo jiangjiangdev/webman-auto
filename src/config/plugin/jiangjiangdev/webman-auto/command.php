4e66b3b098ab129ca46f93c466c14de5aa39cfe3
JiangJiang <jiang@jiang.jiang>
2024-06-30 19:58:03 +0800

    feat: 新增AuthCreateCommand至command.php

--- src/config/plugin/jiangjiangdev/webman-auto/command.php
+++ src/config/plugin/jiangjiangdev/webman-auto/command.php
@@ -1,9 +1,11 @@
 <?php
 
+use Jiangjiangdev\WebmanAuto\Commands\AuthCreateCommand;
 use Jiangjiangdev\WebmanAuto\Commands\ContentResetCommand;
 use Jiangjiangdev\WebmanAuto\Commands\ResourceCreateCommand;
 
 return [
+    AuthCreateCommand::class,
     ContentResetCommand::class,
     ResourceCreateCommand::class,
 ];
